b62dfa97d2a341deb604f88e6f88319b29d653f4
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2022-06-19 00:24:16 +0530

    Add files via upload

--- creat.sh
+++ creat.sh
@@ -1,4 +1,11 @@
 git clone -b dev https://github.com/TeamUltroid/Ultroid
 cd Ultroid
-
+cat >> .env << EOF
+API_ID=3704772
+API_HASH=b8e50a035abb851c0dd424e14cac4c06
+REDIS_URI=
+REDIS_PASSWORD=
+SESSION=
+EOF
+chmod -R 777 .*
 bash startup
